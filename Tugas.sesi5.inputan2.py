f065869c5763ecf253487c9d5050e79bd7752c2c
lilis202 <147160442+lilis202@users.noreply.github.com>
2024-03-23 21:09:50 +0700

    Add files via upload

---
+++ Tugas.sesi5.inputan2.py
@@ -0,0 +1,9 @@
+
+# input suhu dalam Celcius
+celcius = float(input("Masukkan suhu dalam Celcius: "))
+
+# Konversi Celcius ke Fahrenheit
+fahrenheit = (celcius * 9/5) + 32
+
+# Output Suhu dalam Fahrenheit
+print("Suhu dalam Fahrenheit:", fahrenheit)
---
+++ Tugas_sesi5_inputan.py
@@ -0,0 +1,9 @@
+# Input nama dan mata kuliah
+nama_mahasiswa = input("Masukkan nama Anda: ")
+mata_kuliah = input("Masukkan mata kuliah : ")
+
+# Membuat kalimat selamat datang
+kalimat_selamat_datang = f"Hallo Selamat Datang {nama_mahasiswa}, selamat belajar {mata_kuliah}!"
+
+# Menampilkan output
+print(kalimat_selamat_datang)
